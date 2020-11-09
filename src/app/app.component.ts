3fe719dabf56dadfc868718cd49333cc1663553b
clementine <clementine.saintmaxent@gmail.com>
2020-11-09 10:45:26 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularCours';
+  // propriétés
+  title: string = "App Component";
+
+  // méthodes
+  constructor() {
+    console.log("constructor executed");
+
+  }
 }
